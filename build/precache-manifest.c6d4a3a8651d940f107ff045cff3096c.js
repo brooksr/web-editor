78adf56e7844c1d2862b80eb55db8e4c970dba79
broche <broche@upsellit.com>
2023-02-10 11:11:46 -0800

    build

---
+++ build/precache-manifest.c6d4a3a8651d940f107ff045cff3096c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aeb2f9d28645bfa75bf9ff554fc083c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a43dccbe5aa9dcda9e7",
+    "url": "/static/css/2.56a1fa8e.chunk.css"
+  },
+  {
+    "revision": "ecc0a4651cbc603c131a",
+    "url": "/static/css/main.05ddc656.chunk.css"
+  },
+  {
+    "revision": "4a43dccbe5aa9dcda9e7",
+    "url": "/static/js/2.929282e8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.929282e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc0a4651cbc603c131a",
+    "url": "/static/js/main.2b93878b.chunk.js"
+  },
+  {
+    "revision": "e68d2feaab5baf5347e0",
+    "url": "/static/js/runtime-main.23fe08a1.js"
+  }
+]);
